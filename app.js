88a1352433c7f1d5bc49830c153e43f3055358b9
Taha Muslu <taha-_muslu1216@hotmail.com>
2022-08-25 09:57:11 +0300

    public ve views klasörü oluşturuldu

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
+//Requirements
 const express = require('express');
 
 
+
+
+//Express Start
 const app = express();
 
 
 
+//Template Engine
+app.set("view engine", "ejs");
 
 
 
+//Middlewares
+app.use(express.static("public"));
+
+
+
+//Routes
 app.get('/', (req, res) => {
-    res.status(200).send("index");
-})
+    res.status(200).render('index', {
+        page_name: "index"
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+        page_name: "about"
+    });
+});
 
 
 
+
+
+
+//Server Start
 const port = 3000;
 
 app.listen(port, () => {
